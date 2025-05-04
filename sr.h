4e19eded1c19fab460ad9029a1ab7ec92e9f7ee7
Hrithh <a1790387@adelaide.edu.au>
2025-05-05 06:44:30 +0930

    Set SR_WINDOW_SIZE to 6 and retain single-timer-for-base strategy

--- sr.h
+++ sr.h
@@ -13,4 +13,22 @@ extern void B_input(struct pkt packet);
 extern void B_output(struct msg message);
 extern void B_timerinterrupt(void);
 
+/* --- Selective Repeat Parameters & State --- */
+#define SR_WINDOW_SIZE 6        /* size of sender & receiver window */
+#define MAX_SEQ_NUM    32       /* sequence number space (0-31) */
+
+#define NOTINUSE       (-1)     /* ‘acknum’ field value when unused */
+#define TIMEOUT_INTERVAL 15.0    /* timeout interval for each SR packet */
+
+/* Sender‐side state */
+extern struct pkt A_window[SR_WINDOW_SIZE];   /* buffer of sent but unacked packets */
+extern int       A_acknowledged[SR_WINDOW_SIZE]; /* ack flags for each slot */
+extern int       A_base;                        /* lowest unacked seq# */
+extern int       A_nextseqnum;                  /* next seq# to use */
+
+/* Receiver‐side state */
+extern struct pkt B_window[SR_WINDOW_SIZE];   /* buffer of out‐of‐order packets */
+extern int       B_received[SR_WINDOW_SIZE];  /* flags: packet received in slot */
+extern int       B_expected_base;             /* lowest seq# not yet delivered */
+
 #endif  /* SR_H */
